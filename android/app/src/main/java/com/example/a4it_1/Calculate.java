43f3feb5598f266471fd76497e8d1d76e1816549
vstorchak <vadim.storchak@utair.ru>
2021-09-22 04:08:12 +0300

    add some method's

---
+++ android/app/src/main/java/com/example/a4it_1/Calculate.java
@@ -0,0 +1,81 @@
+package com.example.a4it_1;
+
+import android.graphics.Path;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Calculate {
+    @Getter
+    private Double firstOperand;
+    private Double secondOperand;
+    private Double result;
+    private Operations operation;
+
+    public Double addNumber(Double newOperand, String operation) {
+        if (firstOperand == null) {
+            firstOperand = newOperand;
+            return 0d;
+        } else {
+            if (secondOperand == null) {
+                secondOperand = newOperand;
+                return 0d;
+            } else {
+                if (operation != null) {
+                    result = madeOperation(this.operation);
+                    secondOperand = newOperand;
+                    this.operation = convertStringToOperation(operation);
+                    firstOperand = result;
+                    return result;
+                }
+            }
+        }
+        return null;
+    }
+
+    public double madeOperation(Operations operation) {
+        switch (operation) {
+            case plus:
+                return firstOperand + secondOperand;
+            case minus:
+                return firstOperand - secondOperand;
+            case multpl:
+                return firstOperand * secondOperand;
+            case division:
+                if (secondOperand == 0d)
+                    return Double.POSITIVE_INFINITY;
+                else
+                    return firstOperand / secondOperand;
+        }
+        return result;
+    }
+
+    private Operations convertStringToOperation(String operation) {
+        switch (operation) {
+            case "+":
+                return Operations.plus;
+            case "-":
+                return Operations.minus;
+            case "*":
+                return Operations.multpl;
+            case "/":
+                return Operations.division;
+        }
+        return Operations.plus;
+    }
+
+    public void clear() {
+        firstOperand = 0d;
+        secondOperand = null;
+        operation = null;
+        result = null;
+    }
+
+}
+
+enum Operations {
+    plus, minus, multpl, division
+}
--- android/app/src/main/java/com/example/a4it_1/MainActivity.java
+++ android/app/src/main/java/com/example/a4it_1/MainActivity.java
@@ -1,50 +1,62 @@
 package com.example.a4it_1;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.ColorSpace;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
 public class MainActivity extends AppCompatActivity {
+    private Calculate calculate;
+    private Resources resources;
+    private List<Button> operationButtons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_ll);
-
-        View.OnClickListener listener = new View.OnClickListener() {
-            @SuppressLint("SetTextI18n")
-            @Override
-            public void onClick(View view) {
-                String editText = ((EditText) findViewById(R.id.edit1)).getText().toString();
-                ((TextView) findViewById(R.id.edit1)).setText(editText + ((Button) view).getText());
-            }
-        };
-
-        ((Button) findViewById(R.id.b1)).setOnClickListener(listener);
-        ((Button) findViewById(R.id.b2)).setOnClickListener(listener);
-        ((Button) findViewById(R.id.b3)).setOnClickListener(listener);
-        ((Button) findViewById(R.id.b4)).setOnClickListener(listener);
-        ((Button) findViewById(R.id.b5)).setOnClickListener(listener);
+        resources = getResources();
+        initOperationButtons();
+        calculate = new Calculate();
     }
 
-    public void copyClick(View view) {
+    public void numberButtonsClick(View view) {
         String editText = ((EditText) findViewById(R.id.edit1)).getText().toString();
-        ((TextView) findViewById(R.id.edit1)).setText(editText);
+        ((TextView) findViewById(R.id.edit1)).setText(editText + ((Button) view).getText());
     }
 
-    public void plusOne(View view) {
-        String editText = ((EditText) findViewById(R.id.edit1)).getText().toString();
-        Integer editTextNumber;
-        if (editText.isEmpty())
-            editTextNumber = 0;
-        else
-            editTextNumber = Integer.parseInt(editText);
-        editTextNumber++;
-        ((TextView) findViewById(R.id.edit1)).setText(String.valueOf(editTextNumber));
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void operationButtonsClick(View view) {
+        Button clickedButton = (Button) view;
+        clickedButton.setBackgroundColor(resources.getColor(R.color.active_operation));
+
+        operationButtons.forEach(button -> {
+            if (!button.equals(clickedButton))
+                clickedButton.setBackgroundColor(resources.getColor(R.color.main_buttons));
+        });
+
+        Double newOperand = Double.parseDouble(((EditText) findViewById(R.id.edit1)).getText().toString());
+        ((TextView) findViewById(R.id.edit1)).setText(String.valueOf(calculate.addNumber(newOperand, clickedButton.getText().toString())));
+    }
+
+    private void initOperationButtons() {
+        operationButtons = new ArrayList<>(4);
+        // Иницируем кнопки операций, чтобы было проще к ним обращаться
+        operationButtons.add((Button) findViewById(R.id.bPlus));
+        operationButtons.add((Button) findViewById(R.id.bMinus));
+        operationButtons.add((Button) findViewById(R.id.bDivision));
+        operationButtons.add((Button) findViewById(R.id.bMultip));
     }
 }
